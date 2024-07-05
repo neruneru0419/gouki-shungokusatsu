4ff635b939897b510ab406e75ba5e681e94e4a2b
neruneru0419 <neruneru55555@gmail.com>
2024-07-05 21:40:00 +0900

    mainファイル作成

--- main.rb
+++ main.rb
@@ -1,45 +1,36 @@
+require "x"
+
 def convert_reading_vertically(four_character_idioms)
+  result = ""
   fci_length = four_character_idioms.length
   fci_length.times do |i|
     four_character_idioms.reverse.each do |four_character_idiom|
-      print four_character_idiom[i] + "  "
+      result += four_character_idiom[i] + "   "
     end
-    puts ""
+    result += "\n"
   end
+  result
 end
 
-four_character_idioms = [
-  "早寝早起",
-  "顔面崩壊",
-  "諸行無常",
-  "勇気凛々",
-  "元気発剌",
-  "興味津々",
-  "意気揚々",
-  "元気百倍",
-  "奇想天外",
-  "謹賀新年",
-  "七転八倒",
-  "自作自演",
-  "自業自得",
-  "三寒四温",
-  "焼肉定食",
-  "四捨五入",
-  "最低賃金",
-  "業務委託",
-  "定時退社",
-  "一攫千金",
-  "不労所得",
-  "四捨五入",
-  "四捨五入",
-  "四捨五入",
-]
+def post
+  x_credentials = {
+    api_key:             ENV["API_KEY"],
+    api_key_secret:      ENV["API_KEY_SECRET"],
+    access_token:        ENV["ACCESS_TOKEN"],
+    access_token_secret: ENV["ACCESS_TOKEN_SECRET"],
+  }
+  x_client = X::Client.new(**x_credentials)
 
-four_character_idioms = [
-  "一瞬千撃",
-  "抜山蓋世",
-  "鬼哭啾々",
-  "故豪鬼成",
-]
+  four_character_idioms = []
+  File.open("shungoku.txt", "r") do |f|
+    f.each_line do |l|
+      four_character_idioms.push(l.chomp)
+    end
+  end
+  chanting_words = four_character_idioms.sample(3) + ["故豪鬼成"]
+  post_text = convert_reading_vertically(chanting_words) + "\n　👊 👊 👊" + "\n👊　　　　👊" + "\n👊　 👹　 👊" + "\n👊　　　　👊" + "\n　👊 👊 👊"
+  post_data = {text: post_text}
+  puts x_client.post("tweets", post_data.to_json)
+end
 
-tateyomi(four_character_idioms)
+post
